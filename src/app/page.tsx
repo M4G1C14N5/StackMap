d74e5964ce2933099d714420ae57e09c3d818ab6
Thomas Tsangou <thomas@camuedlabs.org>
2026-05-25 07:41:09 +0000

    Nothing renders while it's loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from 'react';
 import StackMapCanvas from '../components/StackMapCanvas';
 import FloatingSidebar from '../components/FloatingSidebar';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col p-0">
-      <FloatingSidebar />
+      <Suspense fallback={null}>
+        <FloatingSidebar />
+      </Suspense>
       <div className="ml-80 w-full h-full">
         <StackMapCanvas />
       </div>
